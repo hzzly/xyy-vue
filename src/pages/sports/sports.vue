8a022defac8b9c22f477aa694e1f1c45ba017f53
hzzly <hjingren@aliyun.com>
2017-02-24 10:57:49 +0800

    添加四个主页面结构

---
+++ src/pages/sports/sports.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="sports">
+		<panel-list :sport-list="sportList"></panel-list>
+	</div>
+</template>
+
+<script>
+
+import panelList from 'components/panelList'
+
+import { mapGetters } from 'vuex'
+
+export default {
+
+	components: {
+		panelList
+	},
+	data() {
+		return {
+			
+		}
+	},
+	computed: {
+		...mapGetters([
+			'sportList'
+		])
+	},
+	mounted(){
+		
+    },
+	methods: {
+		
+	}
+}
+</script>
---
+++ src/pages/travels/travels.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="travels">
+		<travel-list :travel-lists="travelList"></travel-list>
+	</div>
+</template>
+
+<script>
+
+import travelList from 'components/travelList'
+
+import { mapGetters } from 'vuex'
+
+export default {
+
+	components: {
+		travelList
+	},
+	computed: {
+		...mapGetters([
+			'travelList'
+		])
+	},
+	data() {
+		return {
+			
+		}
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+
+.travels {
+	padding-top: 1px;
+}
+</style>
