dcb716e978a51df7b3050917605648f06abc3488
hzzly <hjingren@aliyun.com>
2017-08-28 15:25:15 +0800

    :sparkles: add device adapter

--- build/utils.js
+++ build/utils.js
@@ -20,9 +20,16 @@ exports.cssLoaders = function (options) {
     }
   }
 
+  var px2remLoader = {
+    loader: 'px2rem-loader',
+    options: {
+      remUnit: 75
+    }
+  }
+
   // generate loader string to be used with extract text plugin
   function generateLoaders (loader, loaderOptions) {
-    var loaders = [cssLoader]
+    var loaders = [cssLoader, px2remLoader]
     if (loader) {
       loaders.push({
         loader: loader + '-loader',
